9ed7e1d36be3724b348f64685aee88d213caf870
NataliiaVirt <virtnatalie@gmail.com>
2025-04-28 23:19:19 +0300

    Update_cp.js
    Add spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,29 @@
+import { spawn } from 'child_process';
+import path from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = path.resolve('files', 'script.js');
+
+  try {
+    const child = spawn('node', [scriptPath, ...args], {
+      stdio: ['pipe', 'pipe', 'inherit', 'ipc']
+    });
+
+    process.stdin.pipe(child.stdin);
+
+    child.stdout.pipe(process.stdout);
+
+    child.on('error', (err) => {
+      throw new Error('Failed to spawn child process');
+    });
+
+    child.on('exit', (code) => {
+      console.log(`Child process exited with code ${code}`);
+    });
+
+  } catch (err) {
+    console.error('FS operation failed');
+  }
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess('Hello World!');
